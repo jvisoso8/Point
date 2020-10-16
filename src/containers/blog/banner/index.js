1f76d365d56fc215625ff684fe1f9824f6d2cd81
josevisoso <Jvisoso8@hotmail.com>
2020-10-16 18:37:52 -0500

    init

---
+++ src/containers/blog/banner/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import Image from '../../../components/image'
+
+const Banner = () => {
+
+    return (
+        <div className="banner-area">
+
+        </div>
+    )
+}
+
+export default Banner
