87d544cabeb7521bb5916dd1edea0fb579c7f4ee
josevisoso <Jvisoso8@hotmail.com>
2020-11-05 12:33:58 -0600

    add google analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,21 +108,18 @@ module.exports = {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
                 // The property ID; the tracking code won't be generated without it
-                trackingId: "UA-158680515-1",
+                trackingId: "G-LMH0J4DQ8E",
                 // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: false,
+                head: true,
                 // Setting this parameter is optional
                 anonymize: true,
                 // Setting this parameter is also optional
                 respectDNT: true,
                 // Avoids sending pageview hits from custom paths
-                exclude: ["/preview/**", "/do-not-track/me/too/"],
+                exclude: ["/preview/**"],
                 // Delays sending pageview hits on route update (in milliseconds)
                 pageTransitionDelay: 0,
-                // Any additional optional fields
-                sampleRate: 5,
-                siteSpeedSampleRate: 10,
-                cookieDomain: "example.com",
+
             },
         },
     ]
